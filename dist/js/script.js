9f9b103014e60668d151569fbe33aa4d3548ac99
Yury Sauko <yury.savko@gmail.com>
2023-11-02 12:04:20 +0300

    Added functionality for button Show all above 1120px

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,21 @@ const swiper = new Swiper('.swiper', {
         }
     }
 });
+
+/* Реализация функционала кнопки Показать все / Скрыть */
+let clickToShow = document.querySelector('.show-brands');
+clickToShow.addEventListener('click', showHideEl);
+
+function showHideEl () {
+    let swiperSlideHidden = document.querySelectorAll('.hiddenEl');
+    
+    swiperSlideHidden.forEach( (item, i) => {
+        swiperSlideHidden[i].classList.toggle('hide1120');
+    });
+
+    if (!swiperSlideHidden[0].classList.contains('hide1120')) {
+        clickToShow.classList.add('hide-brands');
+    } else {
+        clickToShow.classList.remove('hide-brands');
+    }
+}
